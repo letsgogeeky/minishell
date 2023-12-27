88e3a105001e46641f50aa9a37f433a152ecbdad
Ramy <ramymohamedmousa@gmail.com>
2023-12-27 18:36:48 +0100

    minor update

--- src/execution/executor.c
+++ src/execution/executor.c
@@ -6,14 +6,16 @@
 /*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 18:43:22 by ramoussa          #+#    #+#             */
-/*   Updated: 2023/12/26 19:59:58 by ramoussa         ###   ########.fr       */
+/*   Updated: 2023/12/27 18:35:26 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell/execution/executor.h"
 
+// TODO: change arguments to be the expected structs
 int	exec_cmd(char *cmd, char **envp)
 {
+	// TODO: handle when path is NULL
 	printf("%s\n", get_path(cmd, envp));
-	return (0);
+	return (EXIT_SUCCESS);
 }
