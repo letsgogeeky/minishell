67692ed95cc3e6f9318a28b025b481c21dd8fba7
Ramy <ramymohamedmousa@gmail.com>
2023-12-30 03:17:56 +0100

    half-assed executor

--- include/minishell/error.h
+++ include/minishell/error.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ramymoussa <ramymoussa@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 18:52:50 by ramymoussa        #+#    #+#             */
-/*   Updated: 2023/12/19 20:16:01 by ramymoussa       ###   ########.fr       */
+/*   Updated: 2023/12/29 21:57:57 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # include <errno.h>
 
 void    print_builtin_error(char *cmd, char *arg, char *msg);
+void	print_execve_error(char *cmd, char *msg);
 
 #endif
