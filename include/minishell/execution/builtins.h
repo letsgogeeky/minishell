973776bcfccbc3552c5ddc0d2a90addd39e15db1
Ramy <ramymohamedmousa@gmail.com>
2024-01-13 20:06:45 +0100

    env command and some half-assed structure for builtins

--- include/minishell/execution/builtins.h
+++ include/minishell/execution/builtins.h
@@ -6,13 +6,25 @@
 /*   By: ramymoussa <ramymoussa@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 18:59:01 by ramymoussa        #+#    #+#             */
-/*   Updated: 2023/12/16 18:59:40 by ramymoussa       ###   ########.fr       */
+/*   Updated: 2024/01/13 19:58:34 by ramymoussa       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include "baselib.h"
+
 int builtins_pwd();
+int builtins_env(char **envp);
+
+int is_builtin(char *cmd);
+int runs_on_parent(char *cmd);
+int exec_builtin(char *cmd, char **envp);
 
 #endif
