4b1ba7b3830776f2404b7d5eb694eba87aed4f4c
Flavia Voicu <fvoicu@1-E-11.42heilbronn.de>
2024-02-12 22:25:14 +0100

    working i guess

--- include/minishell/parsing/lexer.h
+++ include/minishell/parsing/lexer.h
@@ -6,109 +6,55 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 01:25:36 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/02/03 01:35:42 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/02/12 02:09:20 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #ifndef LEXER_H
 // # define LEXER_H
+// typedef enum e_token_type
+// {
+// 	WORD ,
+// 	ASSIGNMENT,
+// 	OPTION,
+// 	DLESS,
+// 	DGREAT,
+// 	LESS,
+// 	GREAT,
+// 	PIPE,
+// 	EOF_TOKEN ,
+// 	ERROR_TOKEN
+// }	t_token_type;
 
 // typedef enum e_state
 // {
-// 	START,
-// 	READ_CMD,
-// 	READ_PIPE,
-// 	READ_INPUT,
-// 	READ_OUTPUT,
-// 	READ_APPEND,
-// 	READ_HEREDOC,
-// 	END_CMD,
-// 	ERROR,
+// 	start,
+// 	in_word,
+// 	in_option,
+// 	in_separator,
+// 	in_dless,
+// 	in_dgreat,
+// 	in_less,
+// 	in_great,
+// 	in_pipe,
+// 	in_assignment,
+// 	in_dqoute,
+// 	in_squote,
+// 	in_escape,
+// 	in_error
 // }	t_state;
-
-// typedef enum e_input_type
-// {
-// 	WORD,
-// 	ASSIGN,
-// 	PIPE,
-// 	LESS,
-// 	GREAT,
-// 	DLESS,
-// 	DGREAT,
-// 	EOF,
-// 	OTHER
-// }	t_type;
 
 // typedef struct s_token
 // {
-// 	t_type	type;
-// 	char	*value;
+// 	t_token_type	type;
+// 	char			*lexeme;
 // }	t_token;
 
 // typedef struct s_lexer
 // {
-// 	t_state		state;
-// 	char		*input;
-// 	t_token		*token;
-// 	int			position;
-
+// 	char	*input_buffer;
+// 	char	*current;
+// 	char	*start;
+// 	t_state	state;
 // }	t_lexer;
-
-
-
-
-// #endif
-
-// #ifndef LEXER_H
-// #define LEXER_H
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <ctype.h>
-
-// typedef enum {
-//   WORD ,
-//   ASSIGNMENT_WORD,
-//   DLESS,
-//   DGREAT,
-//   LESS,
-//   GREAT,
-//   PIPE,
-//   EOF_TOKEN ,
-//   ERROR_TOKEN
-// } TokenType;
-
-// typedef enum e_state{
-
-//   START = 9,
-//   IN_WORD,
-//   IN_DLESS,
-//   IN_DGREAT,
-//   IN_LESS,
-//   IN_GREAT,
-//   IN_PIPE,
-//   IN_ASSIGNMENT_WORD
-// }	t_state;
-
-// typedef struct Token {
-//   TokenType type;
-//   char *lexeme;
-//   int line;
-// } Token;
-
-// typedef struct Lexer {
-//   char *input_buffer;
-//   char *current;
-//   char *start;
-//   int line_number;
-//   t_state state;
-// } Lexer;
-
-// void lexer_init(Lexer *lexer, const char *filename);
-// void lexer_destroy(Lexer *lexer);
-// Token *get_next_token(Lexer *lexer);
-// Token *new_token(int type, Lexer *lexer);
-// Token *new_error_token(Lexer *lexer);
-
 // #endif
--- src/parsing/parser/parsepars.c
+++ src/parsing/parser/parsepars.c
@@ -1,10 +1,12 @@
 //TODO: add libft funcs
-//TODO: create_node
+
 // #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef enum e_token_type
 {
@@ -30,7 +32,6 @@ typedef struct s_token
 
 typedef enum e_ast_node_type
 {
-	N_COMPLETE_COMMAND,
 	N_COMMAND,
 	N_PIPE,
 	N_CMD_WORD,
@@ -38,6 +39,7 @@ typedef enum e_ast_node_type
 	N_CMD_SUFFIX,
 	N_CMD_PARAM,
 	N_INFILE,
+	N_OUTFILE,
 	N_HEREDOC,
 	N_ERROR
 }	t_ast_node_type;
@@ -48,12 +50,11 @@ typedef struct s_ast_node
 {
 	t_ast_node_type		type;
 	t_ast_node 			*parent;
-	t_ast_node 			*left;
-	t_ast_node 			*right;
+	t_ast_node 			*child;
+	t_ast_node			*sibling;
 	char				*data;
-	bool				has_file;
+	bool				is_heredoc;
 	int					fd;
-	int 				line;
 }	t_ast_node;
 
 typedef struct s_parser_state
@@ -61,6 +62,41 @@ typedef struct s_parser_state
 	t_token		*tokens;
 	int			current;
 } t_parser_state;
+
+int error_file_not_found(char *path)
+{
+    // ft_putstr_fd("minishell: ", STDERR_FILENO);
+    // ft_putstr_fd(path, STDERR_FILENO);
+    // ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
+	fprintf(stderr, "minishell: %s: No such file or directory\n", path);
+    return (-1);
+}
+
+int error_permission_denied(char *path)
+{
+    // ft_putstr_fd("minishell: ", STDERR_FILENO);
+    // ft_putstr_fd(path, STDERR_FILENO);
+    // ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
+	fprintf(stderr, "minishell: %s: Permission denied\n", path);
+    return (-1);
+}
+
+int open_file(char *path, int flags)
+{
+    int fd;
+
+    if (flags == O_RDONLY && access(path, F_OK) == -1)
+        return (error_file_not_found(path));
+    if (flags == O_RDONLY && access(path, R_OK) == -1)
+        return (error_permission_denied(path));
+    if (flags > O_RDONLY && !access(path, F_OK) && access(path, W_OK) == -1)
+        return (error_permission_denied(path));
+    if (flags == O_RDONLY)
+        fd = open(path, flags);
+    else
+        fd = open(path, O_WRONLY | O_CREAT | flags, 0644);
+    return (fd);
+}
 
 
 //UTIL FUNCTIONS
@@ -96,6 +132,7 @@ bool	match(t_parser_state *state, t_token_type type)
 	return (false);
 }
 
+// Init funcs
 void	init_parser_state(t_parser_state *state, t_token *tokens)
 {
 	state->tokens = tokens;
@@ -109,167 +146,208 @@ t_ast_node	*create_node(t_ast_node_type type)
 	node = malloc(sizeof(t_ast_node));
 	node->type = type;
 	node->parent = NULL;
-	node->left = NULL;
-	node->right = NULL;
+	node->child = NULL;
+	node->sibling = NULL;
 	node->data = NULL;
-	node->has_file = false;
+	node->is_heredoc = false;
 	node->fd = -1;
-	node->line = -1;
 	return (node);
 }
-t_ast_node_type determine_node_type(t_token_type type) {
-	if (type == WORD)
-		return N_CMD_WORD;
-	else if (type == OPTION)
-		return N_CMD_PARAM;
-	else if (type == ASSIGNMENT_WORD)
-		return N_CMD_PARAM;
-	else if (type == GREAT || type == LESS \
-		|| type == DLESS || type == DGREAT)
-		return N_INFILE;
-	else {
-		printf("Unknown token type encountered.\n");
-		return N_ERROR;
-	}
+void destroy_ast(t_ast_node *node)
+{
+	if (!node)
+		return;
+	destroy_ast(node->child);
+	destroy_ast(node->sibling);
+	free(node->data);
+	free(node);
 }
 
-t_ast_node	*parse_cmd_suffix(t_parser_state *state)
+//Utils
+t_ast_node_type determine_node_type(t_token_type type)
 {
-	t_ast_node	*head = NULL;
-	t_ast_node	*current = NULL;
-	t_ast_node	*node = NULL;
+	if (type == WORD)
+		return (N_CMD_WORD);
+	else if (type == OPTION)
+		return (N_CMD_PARAM);
+	else if (type == ASSIGNMENT_WORD)
+		return (N_CMD_PARAM);
+	else if (type == GREAT || type == LESS \
+		|| type == DLESS || type == DGREAT)
+		return (N_INFILE);
+	else
+		return (printf("Unknown token type encountered.\n"), \
+			N_ERROR);
+}
 
-	while (peek(state).type == ASSIGNMENT_WORD || peek(state).type == GREAT \
-		|| peek(state).type == LESS || peek(state).type == DLESS \
-		|| peek(state).type == DGREAT || peek(state).type == OPTION \
-		|| peek(state).type == WORD)
+bool	is_redirect(t_parser_state *state)
+{
+	t_token_type type;
+
+	type = peek(state).type;
+	return(type == GREAT || type == LESS \
+		|| type == DLESS || type == DGREAT);
+}
+
+t_ast_node *parse_redirect(t_parser_state *state)
+{
+	t_ast_node 		*node;
+	t_token_type	type;
+	t_token			file;
+
+	type = peek(state).type;
+	consume(state);
+	file = consume(state);
+	node = create_node(determine_node_type(type));
+	node->data = strdup(file.lexeme);
+	if (type == GREAT)
+		node->fd = open_file(file.lexeme, O_TRUNC);
+	else if (type == DGREAT)
+		node->fd = open_file(file.lexeme, O_APPEND);
+	else if (type == LESS)
+		node->fd = open_file(file.lexeme, O_RDONLY);
+	else if (type == DLESS)
+		node->is_heredoc = true;
+	if (node->fd == -1)
+		exit(EXIT_FAILURE);
+	return (node);
+}
+
+//TODO:parse_prefix/suffix merge somehow
+
+t_ast_node *parse_cmd_suffix(t_parser_state *state)
+{
+    t_ast_node *head = NULL;
+    t_ast_node *current = NULL;
+    t_ast_node *node = NULL;
+
+	while (peek(state).type == ASSIGNMENT_WORD || is_redirect(state) \
+		|| peek(state).type == OPTION || peek(state).type == WORD)
 	{
-		node = create_node(determine_node_type(peek(state).type));
-		node->data = strdup(peek(state).lexeme);
+		if(is_redirect(state))
+			node = parse_redirect(state);
+		else
+		{
+        	node = create_node(determine_node_type(peek(state).type));
+			node->data = strdup(peek(state).lexeme);
+		}
 		if (!head)
 			head = node;
 		else
-			current->right = node;
+			current->sibling = node;
 		current = node;
 		consume(state);
 	}
 	return (head);
 }
 
-t_ast_node	*parse_cmd_prefix(t_parser_state *state)
+
+t_ast_node *parse_cmd_prefix(t_parser_state *state)
 {
-	printf("going in parse_cmd_prefix\n");
 	t_ast_node *head = NULL;
 	t_ast_node *current = NULL;
 	t_ast_node *node = NULL;
 
-	while(peek(state).type == ASSIGNMENT_WORD || peek(state).type == GREAT \
-		|| peek(state).type == LESS || peek(state).type == DLESS \
-		|| peek(state).type == DGREAT)
+	while(is_redirect(state) || peek(state).type == ASSIGNMENT_WORD)
 	{
-		node = create_node(determine_node_type(peek(state).type));
-		node->data = strdup(peek(state).lexeme);
-		printf("Created node: %d with data %s\n", node->type, node->data);
+		if (is_redirect(state))
+			node = parse_redirect(state);
+		else if (peek(state).type == ASSIGNMENT_WORD)
+		{
+			node = create_node(determine_node_type(peek(state).type));
+			node->data = strdup(peek(state).lexeme);
+		}
 		if (!head)
 			head = node;
 		else
-			current->right = node;
+			current->sibling = node;
 		current = node;
 		consume(state);
 	}
-	return (NULL);
+	return (head);
 }
-t_ast_node	*parse_command(t_parser_state *state)
+
+
+t_ast_node *parse_command(t_parser_state *state)
 {
-	t_ast_node *cmd;
-	t_ast_node *cmd_prefix = NULL;
+	t_ast_node *cmd = create_node(N_COMMAND);
+	t_ast_node *last_child = NULL;
 	t_ast_node *cmd_suffix = NULL;
+	t_ast_node *cmd_prefix = NULL;
 	t_ast_node *cmd_word = NULL;
-	t_token consumed;
 
-	cmd = create_node(N_COMMAND);
-	
-	// Parse cmd_prefix if present
-	if (peek(state).type == ASSIGNMENT_WORD || peek(state).type == GREAT \
-		|| peek(state).type == LESS || peek(state).type == DLESS \
-		|| peek(state).type == DGREAT)
-		cmd_prefix = parse_cmd_prefix(state);
-	// Parse cmd_word if present
-	if (cmd_prefix || peek(state).type == WORD)
+	if (is_redirect(state) || peek(state).type == ASSIGNMENT_WORD)
 	{
-		cmd_word = create_node(N_CMD_WORD);
-		if (!cmd_prefix && peek(state).type == WORD)
-		{
-			cmd_word->data = strdup(peek(state).lexeme);
-			consume(state);
-		}
-		else if (cmd_prefix)
-		{
-			// If there's a prefix, the next token is assumed to be a cmd_word, so consume it
-			consumed = consume(state);
-			cmd_word->data = strdup(consumed.lexeme);
-		}
-printf(" Befilinking prefix:\n");
-printf("Cmd Prefix: %s\n", cmd_prefix ? cmd_prefix->data : "None");
-
-		if (cmd_prefix)
-		{
-			cmd_prefix->right = cmd_word;
-			cmd->left = cmd_prefix;
-		} 
-		else
-			cmd->left = cmd_word;
-printf("After linking prefix:\n");
-printf("Cmd Prefix: %s\n", cmd_prefix ? cmd_prefix->data : "None");
+		cmd_prefix = parse_cmd_prefix(state);
+		cmd->child = cmd_prefix;
+		last_child = cmd_prefix;
+		while (last_child->sibling != NULL)
+			last_child = last_child->sibling;
 	}
-	// Parse cmd_suffix if present
-	if (peek(state).type == ASSIGNMENT_WORD || peek(state).type == GREAT \
-		|| peek(state).type == LESS || peek(state).type == DLESS \
-		|| peek(state).type == DGREAT || peek(state).type == OPTION \
+	if (peek(state).type == WORD)
+	{
+	cmd_word = create_node(N_CMD_WORD);
+		cmd_word->data = strdup(peek(state).lexeme);
+		consume(state);
+		if (last_child)
+			last_child->sibling = cmd_word;
+		else
+			cmd->child = cmd_word;
+		last_child = cmd_word;
+	}
+	if (peek(state).type == ASSIGNMENT_WORD || is_redirect(state) \
 		|| peek(state).type == WORD)
 	{
 		cmd_suffix = parse_cmd_suffix(state);
-		// Directly attach cmd_suffix without conditionals since all branches do the same
-		cmd->right = cmd_suffix;
+		if (last_child)
+			last_child->sibling = cmd_suffix;
+		else
+			cmd->child = cmd_suffix;
 	}
 	return (cmd);
 }
 
 
-t_ast_node	*parse_complete_command(t_parser_state *state)
+
+t_ast_node *parse_complete_command(t_parser_state *state)
 {
-	t_ast_node	*command;
-	t_ast_node	*next_cmd;
-	t_ast_node 	*error;
-	t_ast_node 	*pipe;
+	t_ast_node *command;
+	t_ast_node *pipe;
+	t_ast_node *next_cmd;
+	t_ast_node *error;
 
 	command = parse_command(state);
 	if (match(state, PIPE))
 	{
-		next_cmd = parse_complete_command(state);
 		pipe = create_node(N_PIPE);
-		pipe->data = strdup("|");//rm this
-		pipe->left = command;
-		pipe->right = next_cmd;
+		pipe->data = strdup("|"); //TODO: remove this
+		next_cmd = parse_complete_command(state);
+		pipe->child = command;
+		command->parent = pipe;
+		if (next_cmd)
+		{
+			next_cmd->parent = pipe;
+			command->sibling = next_cmd;
+		}
 		return (pipe);
 	}
+	else if (peek(state).type == EOF_TOKEN)
+		return (command);
 	else
-		if (peek(state).type == EOF_TOKEN)
-			return (command);
-		else
-		{
-			error = create_node(N_ERROR);
-			error->left = command;
-			return (error);
-		}
-		// return (command);
+	{
+		error = create_node(N_ERROR);
+		error->child = command;
+		command->parent = error;
+		return (error);
+	}
 }
+
 
 t_ast_node	*parse_input(const char *input)
 {
-	t_token	*tokens;
+	t_token			*tokens;
 	t_parser_state	state;
-	t_ast_node	*ast;
+	t_ast_node		*ast;
 
 	// tokens = lex(input);
 	init_parser_state(&state, tokens);
@@ -279,8 +357,10 @@ t_ast_node	*parse_input(const char *input)
 }
 
 
-/**************************TEST**************************************/
 
+/********************************************************************/
+/**************************TEST**************************************/
+/********************************************************************/
 
 
 // Mock function to create an array of tokens for testing
@@ -291,14 +371,19 @@ t_token *create_mock_tokens() {
         // {WORD, "echo", 0},
 		// {WORD, "$VAR", 0},
         // {WORD, "Hello, world!", 0},
-		{WORD,"ls", 0},
-		{OPTION,"-l", 0},
-		{PIPE, "|", 0},
-		{WORD,"wc", 0},
-		{PIPE, "|", 0},
-		{WORD,"grep", 0},
+		// {WORD,"ls", 0},
+		// {WORD,"-l", 0},
+		// {WORD,"-a", 0},
+		// {PIPE, "|", 0},
+		// {WORD,"cat", 0},
+		// {PIPE, "|", 0},
+		// {WORD,"wc", 0},
+		// {WORD,"-w", 0},
+		// {WORD,"grep", 0},
 		// {WORD, "grep", 0},
-		{WORD, "test", 0},
+		// {WORD, "test", 0},
+		{LESS, "<", 0},
+		{WORD, "grammar_rules.txt", 0},
         {EOF_TOKEN, NULL, 0}
     };
     return tokens;
@@ -309,9 +394,13 @@ void print_ast(t_ast_node *node, int level)
 	if (node == NULL) return;
 	for (int i = 0; i < level; ++i) printf("  ");
 	printf("%d: %s\n", node->type, node->data ? node->data : "NULL");
-	print_ast(node->left, level + 1);
-	print_ast(node->right, level + 1);
+	printf("fd: %d\n", node->fd);
+	if (node->child)
+		print_ast(node->child, level + 2);
+	if (node->sibling)
+		print_ast(node->sibling, level);
 }
+
 
 int main() {
     t_token *tokens = create_mock_tokens();
@@ -321,8 +410,7 @@ int main() {
     t_ast_node *ast = parse_complete_command(&state);
     print_ast(ast, 0);
 
-    // Don't forget to free your AST and tokens after testing
-    // (not shown here for brevity)
 
+	destroy_ast(ast);
     return 0;
 }
