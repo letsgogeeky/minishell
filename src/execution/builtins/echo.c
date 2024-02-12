b2ad32579dcd875831b7ffd5e58c1c12936eeaf6
Ramy <ramymohamedmousa@gmail.com>
2024-02-12 03:54:02 +0100

    some bug fixes in echo and expander

--- src/execution/builtins/echo.c
+++ src/execution/builtins/echo.c
@@ -6,13 +6,15 @@
 /*   By: ramoussa <ramoussa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 00:44:56 by ramoussa          #+#    #+#             */
-/*   Updated: 2024/02/12 00:46:52 by ramoussa         ###   ########.fr       */
+/*   Updated: 2024/02/12 03:50:56 by ramoussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell/minishell.h"
 #include "minishell/execution/builtins.h"
 #include "minishell/error.h"
+
+void prettify_str(char *str);
 
 void	builtins_echo(char **cmds)
 {
@@ -28,11 +30,84 @@ void	builtins_echo(char **cmds)
 	}
 	while (cmds[i])
 	{
-		printf("%s", cmds[i]);
+		prettify_str(cmds[i]);
 		if (cmds[i + 1])
 			printf(" ");
 		i++;
 	}
 	if (newline)
 		printf("\n");
+}
+
+void	print_without_quotes(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '\'' && str[i] != '\"')
+			printf("%c", str[i]);
+		i++;
+	}
+}
+
+bool is_quoted(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+bool is_wrapped_in_quotes(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\'' || str[i] == '\"')
+	{
+		while (str[i])
+			i++;
+		i--;
+		if (str[i] == '\'' || str[i] == '\"')
+			return (true);
+	}
+	return (false);
+}
+
+void prettify_str(char *str)
+{
+	int	i;
+	bool	is_quoted;
+
+	i = 0;
+	is_quoted = is_wrapped_in_quotes(str);
+	printf("%d\n", is_quoted);
+	if (is_quoted)
+		i++;
+	while (str[i])
+	{
+		if (is_quoted && !str[i + 1])
+			break;
+		if (str[i] == '\\')
+		{
+			if (str[i + 1] == 'n')
+				printf("\n");
+			else if (str[i + 1] == 't')
+				printf("\t");
+			else
+				printf("%c", str[i + 1]);
+			i++;
+		}
+		else
+			printf("%c", str[i]);
+		i++;
+	}
 }
--- src/expander/expander.c
+++ src/expander/expander.c
@@ -9,14 +9,18 @@ void expand(t_minishell *ms)
     int j;
     char *expanded;
     char *tmp;
+	bool	is_quoted;
 
+	is_quoted = false;
     i = -1;
     while (ms->cmds[++i])
     {
         j = -1;
         while (ms->cmds[i][++j])
         {
-            if (ms->cmds[i][j] == '$')
+			if (ms->cmds[i][j] == '\'')
+				is_quoted = !is_quoted;
+            if (ms->cmds[i][j] == '$' && !is_quoted)
             {
 				expanded = expand_variable(ms->cmds[i], j, ms->envp, ms->exit_code);
                 tmp = expanded;
@@ -45,16 +49,32 @@ char	*expand_variable(char *cursor, int j, char **envp, int exit_code)
 	char *expanded;
 	char *tmp;
 	char *value;
+	char *key;
 
 	tmp = ft_substr(cursor, 0, j);
 	if (cursor[j+1] && cursor[j+1] == '?')
 		expanded = ft_strjoin(tmp, ft_itoa(exit_code));
 	else
 	{
-		value = get_env_value(cursor + j + 1, envp);
+		key = get_env_key(cursor + j + 1);
+		value = get_env_value(key, envp);
 		expanded = ft_strjoin(tmp, value);
 		free(value);
+		free(key);
 	}
 	free(tmp);
 	return (expanded);
+}
+
+bool	is_between_single_quotes(char *str, int i)
+{
+	int count;
+
+	count = 0;
+	while (--i >= 0)
+	{
+		if (str[i] == '\'')
+			count++;
+	}
+	return (count % 2);
 }
